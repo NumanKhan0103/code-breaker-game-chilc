88efc27d6d2163301976cd88952540fa99faa6b6
numankhan <numankhannh405@gmail.com>
2023-11-22 15:22:38 +0500

    codecreate livewire component is create , next add logo

---
+++ resources/views/livewire/create-code.blade.php
@@ -0,0 +1,13 @@
+<div>
+
+    <section class="px-6">
+        <div class="container mx-auto">
+            <header class="flex justify-center">
+                <h1>
+                    <img src="" alt="logo">
+                    Code Breacker
+                </h1>
+            </header>
+        </div>
+    </section>
+</div>
